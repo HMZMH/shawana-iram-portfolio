743255d4c1a49b5f2fa96112b6b39ff7c88e8adc
Hamzah Mahmood <hmzmh2020@gmail.com>
2023-09-26 12:27:07 +0100

    Initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='fluid-container'>
+
+      <div className='navigation-menu'>
+        <ul>
+          <li>About Me</li>
+          <li>My Skills</li>
+          <li>My Experience</li>
+          <li>My Education</li>
+          <li>Contact Me</li>
+        </ul>
+      </div>
+
+      <div className='main-title'>
+        <h1>Hi, I'm Shawana!
+          <br/>A Care Coordinator :)</h1>
+      </div>
+      
     </div>
   );
 }
