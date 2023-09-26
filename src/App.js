31280d3be045c07e7994e84531cee14563c8cfb7
Hamzah Mahmood <hmzmh2020@gmail.com>
2023-09-26 19:52:49 +0100

    UI styling and routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
+import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Routes, Link, Outlet } from 'react-router-dom';
+import flower_video from './images/flower_video.mp4';
+import Section1 from './sections/Section1';
 
-function App() {
+function Home() {
   return (
     <div className='fluid-container'>
 
+      <video className='background-video' src={flower_video} autoPlay loop muted />
+
       <div className='navigation-menu'>
         <ul>
-          <li>About Me</li>
+          <li><Link to="/about" className='link'>About Me</Link></li>
           <li>My Skills</li>
           <li>My Experience</li>
-          <li>My Education</li>
           <li>Contact Me</li>
         </ul>
       </div>
 
       <div className='main-title'>
         <h1>Hi, I'm Shawana!
-          <br/>A Care Coordinator :)</h1>
+          <br />A Care Coordinator :)</h1>
       </div>
       
+      <Outlet />
     </div>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about/*" element={<Section1 />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/sections/Section1.js
@@ -0,0 +1,9 @@
+function Section1 () {
+    return(
+        <div style={{maxWidth:'50vw', textAlign:'justify'}}>
+            <p> Having spent my early years mostly in Pakistan and Northern Cyprus, I developed a strong sense of independence, adaptability and care for others. My childhood immersed in diverse cultures laid the foundation for a life of solo travel and service. In my youth, I lived in Mexico, the Sinai desert, Syria, Egypt and Ghana, teaching, volunteering and working with vulnerable populations. These experiences reinforced my calling to dedicate my life to caring for people. </p> <p> After studying Social Work at university and working directly with prison populations, at-risk youth and victims of trauma, I brought my passion for improving lives to the NHS. As a Senior Frailty Care Coordinator in East London, I have been instrumental in successfully implementing a new coordinated care model thanks to my personable communication abilities, tireless work ethic and organisational skills. My advocacy was recognised when the frailty care service received an excellence award. I aim to continue pursuing innovative projects that provide meaningful support and compassionately elevate the lives of the elderly. </p>
+        </div>
+    );
+}
+
+export default Section1;
