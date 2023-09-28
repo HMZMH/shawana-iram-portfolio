fd2303a0e56157ee9d7fd4f5c6f533a41ec5e7f1
Hamzah Mahmood <hmzmh2020@gmail.com>
2023-09-28 16:45:45 +0100

    Styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Route, Routes, Link, Outlet } from 'react-router-dom';
 import flower_video from './images/flower_video.mp4';
 import Section1 from './sections/Section1';
 
-function Home() {
+function App() {
   return (
     <div className='fluid-container'>
 
@@ -13,33 +12,24 @@ function Home() {
 
       <div className='navigation-menu'>
         <ul>
-          <li><Link to="/about" className='link'>About Me</Link></li>
+          <li>About Me</li>
           <li>My Skills</li>
           <li>My Experience</li>
           <li>Contact Me</li>
         </ul>
       </div>
 
-      <div className='main-title'>
-        <h1>Hi, I'm Shawana!
-          <br />A Care Coordinator :)</h1>
-      </div>
+      <section className='sections'>
+        <div className='main-title'>
+          <h1>Hi, I'm Shawana!<br/>A Care Coordinator :)</h1>
+        </div>
+      </section>
       
-      <Outlet />
+      <section className='sections'>
+        <Section1 />
+      </section>
+      
     </div>
-  );
-}
-
-function App() {
-  return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about/*" element={<Section1 />} />
-        </Routes>
-      </div>
-    </Router>
   );
 }
 
